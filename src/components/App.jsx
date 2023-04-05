423c707e678ae090454876edc27a85af6fcc2b35
KlapijcukDima <98531117+KlapijcukDima@users.noreply.github.com>
2023-04-05 15:18:49 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,16 @@
+import { Header } from './Header/Header.jsx';
+import { Title } from './Hero/Title/Title.jsx';
+import { Video } from './Hero/Video/Video.jsx';
+import { Service } from './Main/Service/Service.jsx';
+import { Man } from './Main/Man/Man.jsx';
+import { Footer } from './Footer/Footer.jsx';
+
+
+import jobs from './Data/jobs.json';
+import man from './Data/man.json';
+import service from './Data/service.json';
+ 
+
 export const App = () => {
   return (
     <div></div>

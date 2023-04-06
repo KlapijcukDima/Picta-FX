e4f4babddb7c66047ecec15ea15797ccd54f1f20
KlapijcukDima <98531117+KlapijcukDima@users.noreply.github.com>
2023-04-06 19:52:15 +0300

    Update Man.jsx

--- src/components/Main/Man/Man.jsx
+++ src/components/Main/Man/Man.jsx
@@ -11,7 +11,7 @@ export const Man = ( {name, quote, image, position} ) => {
         </div>
 
         <div className={css.man__right_block}>
-            <img src="https://prommoscow.info/local/templates/main/img/publication/quotes-big.svg" alt="" width='140' height='144'/>
+            <img src="https://uploads-ssl.webflow.com/5f58ebbcd796a811d1406856/5f63afe75619819157b544ca_Asset%206.png" alt="" width='140' height='144'/>
             <p className={css.man__quote}>{quote}</p>
             <h2 className={css.man__name}>{name}</h2>
             <p className={css.man__position}>{position}</p>

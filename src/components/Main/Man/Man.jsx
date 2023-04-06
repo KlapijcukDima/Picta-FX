c011865b31af429a065e266eef06d42aa4f6b136
KlapijcukDima <98531117+KlapijcukDima@users.noreply.github.com>
2023-04-06 16:59:07 +0300

    /

--- src/components/Main/Man/Man.jsx
+++ src/components/Main/Man/Man.jsx
@@ -5,16 +5,13 @@ export const Man = ( {name, quote, image, position} ) => {
     return(
         <section className={css.block}>
         <div className={css.man__img_block}>
-            <use>
-                <svg >
 
-                </svg>
-            </use>
             <img className={css.man__img} src={image} alt={name}/>
 
         </div>
 
         <div className={css.man__right_block}>
+            <img src="https://prommoscow.info/local/templates/main/img/publication/quotes-big.svg" alt="" width='140' height='144'/>
             <p className={css.man__quote}>{quote}</p>
             <h2 className={css.man__name}>{name}</h2>
             <p className={css.man__position}>{position}</p>
